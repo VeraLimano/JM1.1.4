4f1829051ae1b50ac98e68a59533cbf988a8f5e0
VeraLimano <69342940+VeraLimano@users.noreply.github.com>
2020-11-17 18:27:05 +0300

    2

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -23,37 +23,63 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        String sql = "CREATE TABLE IF NOT EXISTS user1 (id BIGINT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20), lastName VARCHAR(20), age TINYINT)";
-        session.createSQLQuery(sql).executeUpdate();
-        System.out.println("Создание таблицы");
+        try {
+            session = util.getSessionFactory().openSession();
+            String sql = "CREATE TABLE IF NOT EXISTS user1 (id BIGINT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20), lastName VARCHAR(20), age TINYINT)";
+            session.createSQLQuery(sql).executeUpdate();
+            System.out.println("Создание таблицы");
+        } catch (Exception throwables) {
+            System.out.println(throwables);
+        }
     }
 
     @Override
     public void dropUsersTable() {
-        session.createSQLQuery("DROP TABLE IF EXISTS user1").executeUpdate();
-        System.out.println("Удаление таблицы");
+        try {
+            session = util.getSessionFactory().openSession();
+            session.createSQLQuery("DROP TABLE IF EXISTS user1").executeUpdate();
+            System.out.println("Удаление таблицы");
+        } catch (Exception throwables) {
+            System.out.println(throwables);
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        User user = new User(name, lastName, age);
-        session.save(user);
-        transaction = session.beginTransaction();
-        transaction.commit();
-        System.out.println("Добавление User(ов) в таблицу");
+        try {
+            session = util.getSessionFactory().openSession();
+            User user = new User(name, lastName, age);
+            session.save(user);
+            transaction = session.beginTransaction();
+            transaction.commit();
+            System.out.println("Добавление User(ов) в таблицу");
+        } catch (Exception throwables) {
+            System.out.println(throwables);
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-        User user = (User) session.get(User.class, id);
-        transaction = session.beginTransaction();
-        session.delete(user);
-        transaction.commit();
-        System.out.println("Удаление User из таблицы");
+        try {
+            session = util.getSessionFactory().openSession();
+            User user = (User) session.get(User.class, id);
+            transaction = session.beginTransaction();
+            session.delete(user);
+            transaction.commit();
+            System.out.println("Удаление User из таблицы");
+        } catch (Exception throwables) {
+            System.out.println(throwables);
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
+        try {
+            session = util.getSessionFactory().openSession();
+        } catch (Exception throwables) {
+            System.out.println(throwables);
+        }
+
         List<User> list = session.createCriteria(User.class).list();
         System.out.println(list);
         System.out.println("Получение всех User(ов) из таблицы");
@@ -62,12 +88,18 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void cleanUsersTable() {
-        transaction = session.beginTransaction();
-        List<User> list = session.createCriteria(User.class).list();
-        for (int i = 0; i < list.size(); i ++) {
-            session.delete(list.get(i));
+
+        try {
+            session = util.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            List<User> list = session.createCriteria(User.class).list();
+            for (int i = 0; i < list.size(); i ++) {
+                session.delete(list.get(i));
+            }
+            transaction.commit();
+            System.out.println("Очистка содержания таблицы");
+        } catch (Exception throwables) {
+            System.out.println(throwables);
         }
-        transaction.commit();
-        System.out.println("Очистка содержания таблицы");
     }
 }
